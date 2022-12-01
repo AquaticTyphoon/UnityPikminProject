05cd5bd059d6754bec7b5a5a5ffd29eaf32c0809
AquaticTyphoon <aquatictyphoon@yahoo.com>
2022-12-01 01:41:21 -0600

    Cursor Motion

--- Player/CursorMove.cs
+++ Player/CursorMove.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class CursorMove : MonoBehaviour
 {
@@ -16,11 +15,12 @@ public class CursorMove : MonoBehaviour
     {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
  
-        if(Physics.Raycast(ray, out RaycastHit raycastHit, playerMask)){
+        if(Physics.Raycast(ray, out RaycastHit raycastHit)){
             transform.position = raycastHit.point;
     
             Vector3 direction = transform.position - OlimarMain.olimarRigidbody.transform.position;
-            transform.rotation = Quaternion.LookRotation(direction);
+            Quaternion bounce =  Quaternion.LookRotation(new Vector3( raycastHit.normal.x, raycastHit.normal.y, raycastHit.normal.z), -direction) * Quaternion.Euler(90, 0, 0);
+            transform.rotation = bounce;
         }
         
     }
--- Player/OlimarMain.cs
+++ Player/OlimarMain.cs
@@ -8,18 +8,17 @@ public class OlimarMain : MonoBehaviour
     public static Rigidbody olimarRigidbody;
     private PlayerInput playerInput;
     private OlimarControl olimarControl;
+    private Vector2 inputVector;
     void Start()
     {
       olimarRigidbody = GetComponent<Rigidbody>();
       playerInput = GetComponent<PlayerInput>();
       olimarControl = new OlimarControl();
       olimarControl.Player.Enable();
-
     }
 
     private void Update(){
       Vector2 inputVector= olimarControl.Player.WASD.ReadValue<Vector2>();
-      float speed = 1f;
       olimarRigidbody.velocity = new Vector3(inputVector.x, olimarRigidbody.velocity.y, inputVector.y);
     }
 }
